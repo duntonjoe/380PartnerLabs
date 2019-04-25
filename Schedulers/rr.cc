dff2c8f3c9422681c740d6b926a7909fac7f0387
Jimmy Roche <jimmyroche@Jimmys-MacBook-Pro.local>
2019-04-25 16:09:04 -0400

    I did some shit with the red robin

--- Schedulers/rr.cc
+++ Schedulers/rr.cc
@@ -8,8 +8,23 @@
 int main(int argc, char *argv[]) {
   Jobs list;
   int time = 0;
-  for(job j : list.getJobs()){
-
+  int maxtime;
+  std::cin >> maxtime;
+  auto collection = list.getJobs();
+  while(collection.size() > 0){
+    auto j = collection[0];
+    if (j.time <= maxtime) {
+      std::cout << time << ",+," << j.tag << std::endl << time << ",R," << j.tag << std::endl;
+      time += j.time;
+      std::cout << time << ",-," << j.tag << std::endl;
+    }
+    else {
+      std::cout << time << ",R," << j.tag << std::endl;;
+      time += maxtime;
+      j.time -= maxtime;
+      collection.push_back(j);
+    }
+    collection.erase(collection.begin());
   }
   return EXIT_SUCCESS;
 }
